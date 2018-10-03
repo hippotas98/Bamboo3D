833b8af71b77cf182b5cf0fa49f3659ef71e4be2
Nhat Huy <nhathuy1202@gmail.com>
2018-10-03 16:51:43 +0700

    Initial Commit

---
+++ LSystem3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LSystem3D
+//
+//  Created by Apple on 10/3/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
